358f53f25217d1ee651409d75d9da977c287d562
Steve <steve.brillantes@gmail.com>
2026-05-21 11:43:53 +0800

    fix: set COLYSEUS_SEAT_RESERVATION_TIME before any imports

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,9 @@
+// MUST be set BEFORE any colyseus import
+process.env.COLYSEUS_SEAT_RESERVATION_TIME = '300'
+
 import { listen } from '@colyseus/tools'
 import app from './app'
+
+console.log('[boot] COLYSEUS_SEAT_RESERVATION_TIME =', process.env.COLYSEUS_SEAT_RESERVATION_TIME)
 
 listen(app)
